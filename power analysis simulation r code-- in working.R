675e7cd28e150919eb9368d2e01b171ba4196b89
Yixian Li <37552596+yixian625@users.noreply.github.com>
2018-12-20 11:46:28 -0500

    Add files via upload

---
+++ power analysis simulation r code-- in working.R
@@ -0,0 +1,164 @@
+
+#use the package knitr to put all the codes and graphs into one document
+#knitr::opts_chunk$set(echo=TRUE, warning=FALSE, message=FALSE)
+
+# guess the pupulation means (mu) for all the conditions
+# estimated mean from pilot data: mean rating across avatars=4.3; mean std=1.5
+
+grand_mean=4.3
+mean_std=1.5
+
+
+# estimated effect size for *disclosure depth* from Collins & Miller (1994) d=.324
+# here we are being conservative and just use a small effect size
+estimated_d=.2
+
+# estimate the marginal means for the high vs. low disclosure depth avatars
+# let marginal mean rating for high-depth avatars be x1;
+# let marginal mean rating for low-depth avatars be x2.
+# get values of x1 and x2 based on:
+# { (x1+x2)/2= grand mean
+#   cohen's d = (x1-x2)/ SD }
+
+marginal_means_given_effect<- function(mean, std, d){
+  x1=(std*d+mean*2)/2
+  x2=(mean*2-std*d)/2
+  return(c(x1, x2))
+}
+
+# calculate the estimated mean for high-depth avatars(mean_hd) and low-depth avatars(mean_ld)
+mean_hd= marginal_means_given_effect(grand_mean, mean_std, estimated_d)[1]
+mean_ld= marginal_means_given_effect(grand_mean, mean_std, estimated_d)[2]
+
+# estimate the means for the three similarity conditions *within* the low disclosure depth level
+# let the low, medium, high, similarity level be x1, x2, x3, respectively
+# assuming a *small* effect size d=0.2, which is the difference between x3 and x1, with x2 lying inbetween
+
+# get the values of x1, x2, x3, based on:
+# { x3= x2+(x2-x1)
+#   (X1+x2+x3)/3= mean_ld
+#   cohen's d=(x3-x1)/sd }
+
+cell_means_given_effect <- function(mean, std, d){
+  x2=mean
+  x1=mean-(std*d/2)
+  x3=2*x2-x1
+  return(c(x1, x2, x3))
+}
+
+# calculate estimated cell means for low-sim, medium-sim, and high-sim avatars within the low depth level 
+# (mean_ld_ls, mean_ld_ms, mean_ld_hs, respectively)
+
+mean_ld_ls=cell_means_given_effect(mean_ld, mean_std, 0.2)[1]
+mean_ld_ms=cell_means_given_effect(mean_ld, mean_std, 0.2)[2]
+mean_ld_hs=cell_means_given_effect(mean_ld, mean_std, 0.2)[3]
+
+# calculate estimated cell means for low-sim, medium-sim, and high-sim avatars within the *high* depth level 
+# (mean_hd_ls, mean_hd_ms, mean_hd_hs, respectively)
+# assuming a *moderate* effect size of similarity d=0.5 within the *high* disclosure depth level
+# this creates an interaction between similarity and disclosure depth on rating
+
+mean_hd_ls=cell_means_given_effect(mean_hd, mean_std, 0.5)[1]
+mean_hd_ms=cell_means_given_effect(mean_hd, mean_std, 0.5)[2]
+mean_hd_hs=cell_means_given_effect(mean_hd, mean_std, 0.5)[3]
+
+
+## following codes adapted from: 
+# https://cognitivedatascientist.com/2015/12/14/power-simulation-in-r-the-repeated-measures-anova-5/
+
+
+# define the parameter
+mu= c(mean_ld_ls, mean_ld_ms, mean_ld_hs, mean_hd_ls, mean_hd_ms, mean_hd_hs) #estimated true population mean
+sigma= 1.5 # population standard deviation
+rho= 0.5 # correlation between repeated measures (within-subject correlation)
+nsubs= 70 # number of subjects
+nsims= 500 # number of similulations to run
+
+# create the two IV factors
+
+condition= data.frame(
+  Sim= rep(c(1, 2, 3), nsubs *2), # define similarity with 3 levels
+  Depth= rep(c(1, 2), nsubs, each=3) # define disclosure depth with 2 levels
+)
+
+head(condition, n=12)
+
+# create a factor for the subject ID
+# define subject number as a factor so r knows that its categorical not ordinal/numeric
+
+subject=factor(sort(rep(1:nsubs, 6))) # each Ss ID is repeated 6 times because they go through 6 conditions
+
+# combine the subject IV factor with the condition matrix
+
+df= data.frame(subject, condition)
+head(df, n=12)
+
+# build a covariance matrix based on the parameters previously defined
+
+# 1. create a 6*6 matrix as we have 6 conditions (containing sd for all conditions)
+
+sigma.mat = rep(sigma, 6)
+S.matrix= matrix(sigma.mat, ncol = length(sigma.mat), nrow = length(sigma.mat))
+S.matrix
+# 2. calculate covariance between each two conditions
+
+Sigma.matrix <- t(S.matrix) * S.matrix * rho 
+Sigma.matrix
+
+# put the variances on the diagonal
+diag(Sigma.matrix) <- sigma^2
+Sigma.matrix
+
+# Running the simulation
+
+# stack all the individual simulated data frames into one long data frame
+df_all= df[rep(seq_len(nrow(df)), nsims), ] # generate a sequence equal the number of rows for each simulation, and repeat that for the number of simulations defined (nsims)
+
+# sort and name the simulation runs as the index
+df_all$simulation = sort(rep(seq_len(nsims), nrow(df)))
+df_all
+
+
+##################### codes I don't understand ##########
+
+
+# sample the observed data from a multivariate normal distribution
+# using MASS::mvrnorm with the parameters mu and Sigma created earlier
+# and bind to the existing df
+
+
+require(MASS)
+
+set.seed(1234) # set a seed for the following simulation so that the results would always be the same
+
+make.y = expression(as.vector(t(mvrnorm(nsubs, mu, Sigma.matrix))))
+df_all$y = as.vector(replicate(nsims, eval(make.y)))             
+
+# use do(), the general purpose complement to the specialized data 
+# manipulation functions available in dplyr, to run the ANOVA on
+# each section of the grouped data frame created by group_by
+
+#install.packages("car")
+#install.packages('backports')
+#install.packages("broom")
+
+require(dplyr)
+require(car)
+require(broom)
+
+mods <- df_all %>% 
+  group_by(simulation) %>% 
+  do(model = aov(y ~ Sim * Depth + Error(subject / (Sim*Depth)), qr=FALSE, data = .)) 
+
+
+# extract p-values for each effect and store in a data frame
+p = data.frame(
+  mods %>% do(as.data.frame(tidy(.$model[[3]])$p.value[1])),
+  mods %>% do(as.data.frame(tidy(.$model[[4]])$p.value[1])),
+  mods %>% do(as.data.frame(tidy(.$model[[5]])$p.value[1])))
+colnames(p) = c('Sim','Depth','Interaction')
+
+
+power = apply(as.matrix(p), 2, 
+              function(x) round(mean(ifelse(x < .05, 1, 0) * 100),0))
+power
